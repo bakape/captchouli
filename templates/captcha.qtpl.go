fc864e23e7c420f4b631f93da5b2669b9b1fb0e9
bakape <scorpid33@gmail.com>
2018-11-09 21:20:53 +0200

    Downscale to screen size for mobile
    close #10

--- templates/captcha.qtpl.go
+++ templates/captcha.qtpl.go
@@ -23,87 +23,87 @@ var (
 //line captcha.qtpl:3
 func StreamCaptcha(qw422016 *qt422016.Writer, colour, background, tag string, id [64]byte, images [9][16]byte) {
 	//line captcha.qtpl:3
-	qw422016.N().S(`<style>.captchouli-checkbox {display: none;}.captchouli-checkbox:checked ~ .captchouli-img {transform: scale(0.8);}.captchouli-img {margin: 2px;user-select: none;}.captchouli-width {width: 462px;}.captchouli-margin {margin: 4px 0;}</style><form method="post" class="captchouli-width" style="height:525px; background:`)
-	//line captcha.qtpl:22
+	qw422016.N().S(`<style>.captchouli-checkbox {display: none;}.captchouli-checkbox:checked ~ .captchouli-img {transform: scale(0.8);}.captchouli-img {margin: 2px;user-select: none;max-width: calc((100% - 12px) / 3);max-height: calc((100% - 12px) / 3);}.captchouli-width {width: 462px;}.captchouli-form {height: 525px}.captchouli-margin {margin: 4px 0;}@media screen and (max-width: 462px) {.captchouli-width {max-width: 100%;}.captchouli-form {position: fixed;z-index: 1000;}.captchouli-margin {margin: 0;}}@media screen and (max-height: 525px) {.captchouli-form {overflow-y: scroll;position: fixed;z-index: 1000;max-height: 100%;}.captchouli-margin {margin: 0;}}</style><form method="post" class="captchouli-width captchouli-form" style="background:`)
+	//line captcha.qtpl:50
 	qw422016.E().S(background)
-	//line captcha.qtpl:22
+	//line captcha.qtpl:50
 	qw422016.N().S(`; color:`)
-	//line captcha.qtpl:22
+	//line captcha.qtpl:50
 	qw422016.E().S(colour)
-	//line captcha.qtpl:22
+	//line captcha.qtpl:50
 	qw422016.N().S(`; font-family:Sans-Serif;"><input type="text" name="`)
-	//line captcha.qtpl:23
+	//line captcha.qtpl:51
 	qw422016.N().S(common.IDKey)
-	//line captcha.qtpl:23
+	//line captcha.qtpl:51
 	qw422016.N().S(`" hidden value="`)
-	//line captcha.qtpl:23
+	//line captcha.qtpl:51
 	streamencodeID(qw422016, id)
-	//line captcha.qtpl:23
+	//line captcha.qtpl:51
 	qw422016.N().S(`"><input type="text" name="`)
-	//line captcha.qtpl:24
+	//line captcha.qtpl:52
 	qw422016.N().S(common.ColourKey)
-	//line captcha.qtpl:24
+	//line captcha.qtpl:52
 	qw422016.N().S(`" hidden value="`)
-	//line captcha.qtpl:24
+	//line captcha.qtpl:52
 	qw422016.E().S(colour)
-	//line captcha.qtpl:24
+	//line captcha.qtpl:52
 	qw422016.N().S(`"><input type="text" name="`)
-	//line captcha.qtpl:25
+	//line captcha.qtpl:53
 	qw422016.N().S(common.BackgroundKey)
-	//line captcha.qtpl:25
+	//line captcha.qtpl:53
 	qw422016.N().S(`" hidden value="`)
-	//line captcha.qtpl:25
+	//line captcha.qtpl:53
 	qw422016.E().S(background)
-	//line captcha.qtpl:25
+	//line captcha.qtpl:53
 	qw422016.N().S(`"><header class="captchouli-width captchouli-margin" style="text-align:center; font-size:130%; overflow:auto;">Select all images of <b>`)
-	//line captcha.qtpl:27
+	//line captcha.qtpl:55
 	qw422016.E().S(tag)
-	//line captcha.qtpl:27
-	qw422016.N().S(`</b></header><div class="captchouli-width" style="height:462px;">`)
-	//line captcha.qtpl:30
+	//line captcha.qtpl:55
+	qw422016.N().S(`</b></header><div class="captchouli-width">`)
+	//line captcha.qtpl:58
 	buf := make([]byte, 4096)
 
-	//line captcha.qtpl:31
+	//line captcha.qtpl:59
 	for i, img := range images {
-		//line captcha.qtpl:31
+		//line captcha.qtpl:59
 		qw422016.N().S(`<label><input type="checkbox" name="captchouli-`)
-		//line captcha.qtpl:33
+		//line captcha.qtpl:61
 		qw422016.N().D(i)
-		//line captcha.qtpl:33
+		//line captcha.qtpl:61
 		qw422016.N().S(`" class="captchouli-checkbox"><img class="captchouli-img" draggable="false" src="`)
-		//line captcha.qtpl:34
+		//line captcha.qtpl:62
 		streamthumbnail(qw422016, img, buf)
-		//line captcha.qtpl:34
+		//line captcha.qtpl:62
 		qw422016.N().S(`"></label>`)
-		//line captcha.qtpl:36
+		//line captcha.qtpl:64
 	}
-	//line captcha.qtpl:36
+	//line captcha.qtpl:64
 	qw422016.N().S(`</div><input type="submit" class="captchouli-width captchouli-margin"></form>`)
-//line captcha.qtpl:40
+//line captcha.qtpl:68
 }
 
-//line captcha.qtpl:40
+//line captcha.qtpl:68
 func WriteCaptcha(qq422016 qtio422016.Writer, colour, background, tag string, id [64]byte, images [9][16]byte) {
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	StreamCaptcha(qw422016, colour, background, tag, id, images)
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	qt422016.ReleaseWriter(qw422016)
-//line captcha.qtpl:40
+//line captcha.qtpl:68
 }
 
-//line captcha.qtpl:40
+//line captcha.qtpl:68
 func Captcha(colour, background, tag string, id [64]byte, images [9][16]byte) string {
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	WriteCaptcha(qb422016, colour, background, tag, id, images)
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	qs422016 := string(qb422016.B)
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line captcha.qtpl:40
+	//line captcha.qtpl:68
 	return qs422016
-//line captcha.qtpl:40
+//line captcha.qtpl:68
 }
